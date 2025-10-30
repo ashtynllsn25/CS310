1faf49365e8969a4f465352bec161a8fe5898fb9
Ashtyn Ellison <ashtynllsn20@gmail.com>
2025-10-30 15:17:41 -0500

    Add files via upload

---
+++ Module8Troubleshoot.cpp
@@ -0,0 +1,63 @@
+// ---- Ashtyn Ellison - Programming with C++ ---
+// --- Module 8 Troubleshooting Discussion ---
+
+#include <iostream>
+using namespace std;
+
+// enum for pet types
+enum PetType { DOG, CAT, FISH, BIRD };
+
+// Class #1: Pet
+class Pet {
+public:
+    string name;
+    PetType type;
+
+    // set pet info
+    void setPetInfo(string petName, PetType petType) {
+        name = petName;
+        type = petType;
+    }
+
+    // print pet info
+    void displayPet() {
+        cout << "Pet name: " << name << endl;
+        cout << "Pet type: " << type << endl; // (enum prints as number, that's ok)
+    }
+};
+
+// Class #2: PetManager
+class PetManager {
+public:
+    Pet storedPet; 
+
+    void addPet() {
+        string userPetName;
+        int userTypeChoice;
+
+        cout << "Enter your pet's name: ";
+        cin >> userPetName;
+
+        cout << "Pick your pet type (0=Dog, 1=Cat, 2=Fish, 3=Bird): ";
+        cin >> userTypeChoice;
+
+        storedPet.setPetInfo(userPetName, (PetType)userTypeChoice);
+    }
+
+    void showPet() {
+        storedPet.displayPet();
+    }
+};
+
+class WrongPlacement {
+public:
+int main() {   // <-- main should NOT be inside a class
+    PetManager manager;
+    
+    manager.addPet();
+
+    manager.showPet; // should be manager.showPet();
+
+    return 0;
+}
+}; 
